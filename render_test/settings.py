e637a617f3f88c6aee9f096b1d97e5dcbd9393dc
kimjoemaina <joemainak@gmail.com>
2022-11-24 21:45:27 +0300

    whatever men

--- render_test/settings.py
+++ render_test/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2_76ty5j%u2ctid3c0*%mb*$)2_k(j(e=jrv*y^9br7d7d%*=%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['test-render-y6al.onrender.com']
 
 
 # Application definition
